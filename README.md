9f17563704fda8681dd817856b2a071fef190121
lucasssantospy50-ai <lucasssantospy50@gmail.com>
2025-11-15 18:29:21 -0300

    Upando o Projeto

---
+++ README.md
@@ -0,0 +1,27 @@
+# Projeto de Backup 💾 (⚠EM ANDAMENTO⚠)
+
+## índice 🏹
+  - <a href="#contextualização-do-projeto">Contextualização do Projeto</a>
+  - <a href="#links-para-me-contatar-">Sobre Mim</a>
+  - <a href="#bibliotecas-utiluzadas">Bibliotecas Utilizadas</a>
+
+## Contextualização do Projeto
+Nesse projeto eu quis treinar meus conhecimentos em manipulação de arquivos no computador e, ao mesmo tempo, resolver um problema comum: fazer backups de segurança. Muitas vezes a gente esquece de fazer backup ou acaba não fazendo por ser demorado, então pensei em criar um projeto em Python que automatiza esse processo.
+
+A ideia é simples: o usuário escolhe as pastas e arquivos que deseja incluir no backup e depois define o local onde o backup será salvo. A partir daí, nas próximas execuções, o computador faz tudo automaticamente, usando os caminhos que já ficaram salvos. Se, no futuro, você adicionar novos arquivos ou mover algum arquivo/pasta de lugar, basta atualizar a lista de itens que devem ser salvos. Caso queira começar um backup totalmente novo, com outros caminhos, é só selecionar essa opção no início do programa.
+
+Assim, o processo de backup fica mais rápido, automático e menos sujeito ao esquecimento.
+
+## Bibliotecas utiluzadas
+```python
+import os
+from tkinter import messagebox, filedialog
+from pathlib import Path
+import shutil
+import zipfile
+import tkinter as tk
+import sys
+```
+## Links para me contatar 🔗 
+[![github](https://img.shields.io/badge/github-000?style=for-the-badge&logo=github&logoColor=white)](https://github.com/lucassantospy50-ai)
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/lucas-santos-428869398/)
---
+++ main.py
@@ -0,0 +1,61 @@
+#Projeto Backup
+import os
+from tkinter import messagebox, filedialog
+from pathlib import Path
+import shutil
+import zipfile
+import tkinter as tk
+import sys
+
+root = tk.Tk()
+root.withdraw()
+
+#Deseja fazer o backup
+fazer_backup = messagebox.askyesno('Backup Automáico', 'Opa! Deu a hora do seu backup automático. Quer fazer agora?')
+# padronizar para sempre começar na home
+home = os.path.expanduser("~")
+
+#Pegar as pastas que o usuário deseja compactar
+if fazer_backup:
+    lista_diretorios = []
+    while True:
+        diretorio = filedialog.askdirectory(title= 'Selecione as pastas que você deseja salvar no Backup', initialdir= home)
+        #fechar o programa caso a pessoa clique no X
+        if not diretorio:
+            root.destroy()
+            sys.exit()
+        if diretorio not in lista_diretorios:
+            lista_diretorios.append(diretorio)
+        parar_pasta = messagebox.askyesno('Gerando os Caminhos', 'Precisa fazer backup em mais um diretório?')
+        if not parar_pasta:
+            break
+
+#Pegar os arquivos que ele deseja compactar
+    lista_arquivos = []
+    while True:
+        arquivo = filedialog.askopenfile(title= 'Selecione os arquivos que você deseja salvar no Backup', initialdir= home)
+        if not arquivo:
+            root.destroy()
+            sys.exit()
+        if arquivo not in lista_arquivos:
+            lista_arquivos.append(arquivo)
+        parar_arquvivo = messagebox.askyesno('Gerando os Caminhos', 'Precisa fazer backup em mais um arquivo?')
+        if not parar_arquvivo:
+            break
+
+#Pegar o local onde ele deseja salvar o backup
+    while True:
+        local_backup = filedialog.askdirectory(title='Selecione onde você deseja salvar o backup', initialdir= home)
+        if local_backup:
+            confirmar_backup = messagebox.askyesno('Salvando Caminho', f'Deseja salvar o backup no local {local_backup}?')
+            if confirmar_backup:
+                messagebox.showinfo('Caminho Salvo', f'Local de Backup salvo em {local_backup}')
+                break
+        else:
+            root.destroy()
+            sys.exit()
+
+#Fazer o Backup 
+
+
+root.destroy()
