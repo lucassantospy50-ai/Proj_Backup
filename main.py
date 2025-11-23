2d48c4d8a85a6f2013549e05094d175739007d7b
lucasssantospy50-ai <lucasssantospy50@gmail.com>
2025-11-23 19:58:35 -0300

    Implementa rotina de backup: cria pasta 'Backup' e move arquivos/pastas para lá

--- main.py
+++ main.py
@@ -18,30 +18,34 @@ home = os.path.expanduser("~")
 #Pegar as pastas que o usuário deseja compactar
 if fazer_backup:
     lista_diretorios = []
-    while True:
-        diretorio = filedialog.askdirectory(title= 'Selecione as pastas que você deseja salvar no Backup', initialdir= home)
-        #fechar o programa caso a pessoa clique no X
-        if not diretorio:
-            root.destroy()
-            sys.exit()
-        if diretorio not in lista_diretorios:
-            lista_diretorios.append(diretorio)
-        parar_pasta = messagebox.askyesno('Gerando os Caminhos', 'Precisa fazer backup em mais um diretório?')
-        if not parar_pasta:
-            break
+    confirmar_pastas = messagebox.askyesno('Criando Backup', 'Deseja fazer backup de pastas?')
+    if confirmar_pastas:
+        while True:
+            diretorio = filedialog.askdirectory(title= 'Selecione as pastas que você deseja salvar no Backup', initialdir= home)
+            #fechar o programa caso a pessoa clique no X
+            if not diretorio:
+                root.destroy()
+                sys.exit()
+            if diretorio not in lista_diretorios:
+                lista_diretorios.append(diretorio)
+            parar_pasta = messagebox.askyesno('Gerando os Caminhos', 'Precisa fazer backup em mais um diretório?')
+            if not parar_pasta:
+                break
 
 #Pegar os arquivos que ele deseja compactar
     lista_arquivos = []
-    while True:
-        arquivo = filedialog.askopenfile(title= 'Selecione os arquivos que você deseja salvar no Backup', initialdir= home)
-        if not arquivo:
-            root.destroy()
-            sys.exit()
-        if arquivo not in lista_arquivos:
-            lista_arquivos.append(arquivo)
-        parar_arquvivo = messagebox.askyesno('Gerando os Caminhos', 'Precisa fazer backup em mais um arquivo?')
-        if not parar_arquvivo:
-            break
+    confirmar_arquivos = messagebox.askyesno('Criando Backup', 'Deseja fazer backup de arquivos?')
+    if confirmar_arquivos:
+        while True:
+            arquivo = filedialog.askopenfile(title= 'Selecione os arquivos que você deseja salvar no Backup', initialdir= home)
+            if not arquivo:
+                root.destroy()
+                sys.exit()
+            if arquivo not in lista_arquivos:
+                lista_arquivos.append(arquivo.name)
+            parar_arquvivo = messagebox.askyesno('Gerando os Caminhos', 'Precisa fazer backup em mais um arquivo?')
+            if not parar_arquvivo:
+                break
 
 #Pegar o local onde ele deseja salvar o backup
     while True:
@@ -55,7 +59,32 @@ if fazer_backup:
             root.destroy()
             sys.exit()
 
-#Fazer o Backup 
+#Fazer o Backup dos arquivos 
+    try:
+        if Path(local_backup / Path('Backup')).exists():
+            for arquivo in lista_arquivos:
+                shutil.copy2(arquivo, Path(local_backup / Path('Backup')))
+        else:
+            Path(local_backup / Path('Backup')).mkdir()
+            for arquivo in lista_arquivos:
+                shutil.copy2(arquivo, Path(local_backup / Path('Backup')))
+    except:
+        messagebox.showerror('Backup Automáico', 'Ocorreu um erro no Backup')
 
+#Fazer o Backup dos diretórios 
+    try:
+        if Path(local_backup / Path('Backup')).exists():
+            for diretorio in lista_diretorios:
+                nome_diretorio = Path(diretorio).name
+                novo_caminho = Path(local_backup / Path('Backup')) / Path(nome_diretorio)
+                shutil.copytree(diretorio, novo_caminho)
+        else:
+            Path(local_backup / Path('Backup')).mkdir()
+            for diretorio in lista_diretorios:
+                nome_diretorio = Path(diretorio).name
+                novo_caminho = Path(local_backup / Path('Backup')) / Path(nome_diretorio)
+                shutil.copytree(diretorio, novo_caminho)
+    except:
+        messagebox.showerror('Backup Automáico', 'Ocorreu um erro no Backup')
 
 root.destroy()
